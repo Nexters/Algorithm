79edb87576fa06282d876dfe2cf1428c1dec5ca2
Kang_Taewoo <taewookang96@gmail.com>
2021-03-06 20:00:24 +0900

    [SOLVE] signal.py

--- Baekjoon/H0use96_signal.py
+++ Baekjoon/H0use96_signal.py
@@ -1,89 +1,50 @@
-# 0: 0, 1, 6, 8
-# 1: 2, 5, 9
-# 2: 3, 4
-# 3: 7
-
-TESTCASE = {
-    "size": 40,
-    "data": "###..#..#.#..#..###..#..#.#..#..###..#.."
-}
-
-size = int(TESTCASE["size"])
-data = str(TESTCASE["data"])
-line = int(size/5)
+NUMBERS = [
+    ["0", "###", "#.#", "#.#", "#.#", "###"],
+    ["2", "###", "..#", "###", "#..", "###"],
+    ["3", "###", "..#", "###", "..#", "###"],
+    ["4", "#.#", "#.#", "###", "..#", "..#"],
+    ["5", "###", "#..", "###", "..#", "###"],
+    ["6", "###", "#..", "###", "#.#", "###"],
+    ["7", "###", "..#", "..#", "..#", "..#"],
+    ["8", "###", "#.#", "###", "#.#", "###"],
+    ["9", "###", "#.#", "###", "..#", "###"],
+]
 
 
-def findBlank(lineIdx):
-    blankIdx = []
+def compareNumber(startIdx, line, number):
     for i in range(0, 5):
-        if data[lineIdx + (i*line)] == ".":
-            blankIdx.append(i)
-    return blankIdx
+        idx = startIdx + i * line
+        curStr = data[idx:idx + 3]
+        if curStr != number[i + 1]:
+            return False
+    return True
 
-def main():
-    result = ""
+
+def isNumberOne(startIdx):
+    return data[startIdx] == "#"
+
+
+if __name__ == '__main__':
+
+    size = int(input())
+    data = input()
+    totalLine = int(size / 5)
+
     curLine = 0
-    while curLine < size:
-        blankList = findBlank(curLine)
-        blankCount = len(blankList)
+    result = ""
+    while curLine < totalLine:
+        for i in range(0, 10):
+            if i == 9:
+                if isNumberOne(curLine):
+                    result += "1"
+                    curLine += 2
+                    break
+                else:
+                    curLine += 1
+                    break
 
-        if blankCount == 0:
-            curLine += 1
-            blankList = findBlank(curLine)
-            blankCount = len(blankList)
-
-            if blankCount == 3:
-                result += "0"
-                curLine += 3
-                continue
-
-            if blankCount == 5:
-                result += "1"
-                curLine += 1
-                continue
-
-            if data[curLine + blankList[0] * line + 1] == ".":
-                result += "6"
-                curLine += 3
-                continue
-
-            else:
-                result += "8"
-                curLine += 3
-                continue
-
-        if blankCount == 1:
-            if blankList[0] == 1:
-                result += "2"
-                curLine += 3
-                continue
-
-            blankList = findBlank(curLine + 2)
-            blankCount = len(blankList)
-
-            if blankCount == 0:
-                result += "9"
-                curLine += 3
-                continue
-
-            else:
-                result += "5"
-                curLine += 3
-                continue
-
-        if blankCount == 2:
-            if blankList[1] - blankCount[0] == 1:
-                result += "4"
-                curLine += 3
-                continue
-            else:
-                result += "3"
-                curLine += 3
-                continue
-        else:
-            result += "7"
-            curLine += 3
-            continue
-    return result
-
-print(main())
+            if compareNumber(curLine, totalLine, NUMBERS[i]):
+                result += NUMBERS[i][0]
+                curLine += 4
+                break
+    print(result)
