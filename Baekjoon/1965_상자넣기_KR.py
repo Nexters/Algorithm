7599940f11afb367d92aa3ef9c5a6b4f8f4f66ea
angrycatjenny <angrycatjenny@gmail.com>
2021-03-28 21:38:22 +0900

    3월 넷째주

--- Baekjoon/1965_상자넣기_KR.py
+++ Baekjoon/1965_상자넣기_KR.py
@@ -1,8 +1,8 @@
 n = int(input())
 box = list(map(int,input().split()))
-dp = [0 for _ in range(n)]
+dp = [1 for _ in range(n)]
 for i in range(1,n):
     for j in range(i):
         if box[i] > box[j]:
             dp[i] = max(dp[i],dp[j]+1) #box[i]보다 작은 box[j]가 담을 수 있는 최대 상자의 수
-print(max(dp)+1)        
+print(max(dp))
